7f101a6682561f5524a80f515f351c4670d9ddb6
Razvan Bielz <razvan.bielz@gmail.com>
2023-09-11 16:57:26 +0300

    add login to service & store
    add loginRequest interface

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -5,6 +5,7 @@ import { Observable, map } from 'rxjs';
 import { CurrentUserInterface } from 'src/app/shared/types/currenUser.interface';
 import { AuthResponseInterface } from '../types/authResponse.interface';
 import { environment } from 'src/environments/environment';
+import { LoginRequestInterface } from '../types/loginRequest.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -12,11 +13,23 @@ import { environment } from 'src/environments/environment';
 export class AuthService {
   constructor(private http: HttpClient) {}
 
+  getUser(response: AuthResponseInterface): CurrentUserInterface {
+    return response.user;
+  }
+
   register(data: RegisterRequestInterface): Observable<CurrentUserInterface> {
     const url = environment.API_URL + '/users';
 
     return this.http
       .post<AuthResponseInterface>(url, data)
-      .pipe(map((res) => res.user));
+      .pipe(map(this.getUser));
+  }
+
+  login(data: LoginRequestInterface): Observable<CurrentUserInterface> {
+    const url = environment.API_URL + '/users/login';
+
+    return this.http
+      .post<AuthResponseInterface>(url, data)
+      .pipe(map(this.getUser));
   }
 }
