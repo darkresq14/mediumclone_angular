7f6d58cd8f174ddbac74f42e4141af968818976e
Razvan Bielz <razvan.bielz@gmail.com>
2023-09-15 10:18:15 +0300

    add article shared service & article store

---
+++ src/app/article/components/article/article.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-article',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './article.component.html',
+  styles: [
+  ]
+})
+export class ArticleComponent {
+
+}
