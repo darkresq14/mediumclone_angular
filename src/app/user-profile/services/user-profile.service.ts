c29158bbd39aeaa8b889780e40b7e66989701c4c
Razvan Bielz <razvan.bielz@gmail.com>
2023-09-19 13:30:04 +0300

    add user-profile store and service

---
+++ src/app/user-profile/services/user-profile.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { UserProfileInterface } from '../types/userProfile.interface';
+import { Observable, map } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { GetUserProfileResponseInterface } from '../types/getUserProfileResponse.interface';
+
+@Injectable()
+export class UserProfileService {
+  constructor(private http: HttpClient) {}
+
+  getUserProfile(slug: string): Observable<UserProfileInterface> {
+    const url = `${environment.API_URL}/profiles/${slug}`;
+
+    return this.http
+      .get<GetUserProfileResponseInterface>(url)
+      .pipe(map((response) => response.profile));
+  }
+}
